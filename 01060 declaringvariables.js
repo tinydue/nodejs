117cd89458fb248b95bc8c95c4343e12fbcb7eb0
Zachary T Fenton <zacharytfenton@hotmail.com>
2020-06-07 14:06:15 -0700

    CommandLine

---
+++ 01060 declaringvariables.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//declare the variable message and assign the variable to it
+let message = 'Hello';
+
+//print message
+console.log(message);
+
+//assigning new value to existing variable 'message'
+message = 'how are you?';
+
+console.log(message);
+
+let momma = 8;
+
+console.log(momma);
---
+++ commandline.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let greeting = 'hello';
+
+let name = process.argv[2];
+
+console.log(greeting, name);

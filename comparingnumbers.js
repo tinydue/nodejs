246bce6cfcb3531b2db1fc445b6b7a91928bbdea
Zachary T Fenton <zacharytfenton@hotmail.com>
2020-06-08 17:09:32 -0700

    Created

---
+++ comparingnumbers.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let daysInWeek = 7;
+
+console.log("daysInWeek == 6", daysInWeek == 6);
+console.log("daysInWeek < 8", daysInWeek < 8);
+console.log("daysInWeek > 8", daysInWeek > 8);
+console.log("daysInWeek <= 8", daysInWeek <= 8);
+console.log("daysInWeek >= 8", daysInWeek >= 8);
+console.log("daysInWeek != 6", daysInWeek != 6);
